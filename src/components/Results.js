fed6311c90e74dfbdae2b110479b62ff2093065a
shaanrahman123 <coderraufur@gmail.com>
2023-09-08 00:41:05 +0530

    updated

--- src/components/Results.js
+++ src/components/Results.js
@@ -5,17 +5,14 @@ import "../App.css";
 function Results({ searchResults }) {
     return (
         <div className="container">
-            <h1 className="result-heading">Search Results For "{searchResults[0].heading.split(":")[0]}"</h1>
-            <div className="result-box">
-                {searchResults.map((result, index) => (
-                    <VideoCard
-                        key={index}
-                        video={result.video}
-                        heading={result.heading}
-                        tags={result.tags}
-                    />
-                ))}
-            </div>
+            {searchResults.map((result, index) => (
+                <VideoCard
+                    key={index}
+                    video={result.video}
+                    heading={result.heading}
+                    tags={result.tags}
+                />
+            ))}
         </div>
     );
 }
